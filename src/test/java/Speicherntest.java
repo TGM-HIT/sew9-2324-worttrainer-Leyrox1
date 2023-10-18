3307e5e3cbdcf2a61a35fb96d88df3325fc7f5c6
Luka Condric <condricluka14@gmail.com>
2023-10-18 16:48:25 +0200

    Tests for: - Wort.java - Worttrainer.java

---
+++ src/test/java/Speicherntest.java
@@ -0,0 +1,43 @@
+import project.Wort;
+import project.Worttrainer;
+import project.LoadAndSave;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+/**
+ * Tests the function of the LoadAndSave class
+ * @author Luka Condric
+ * @version 2023-10-18
+ */
+public class Speicherntest {
+    @DisplayName ("Tests if the Worttrainer is loaded correctly")
+    @Test
+    public void testLoad() throws IOException {
+        Worttrainer trainer = new Worttrainer();
+        LoadAndSave loadandsave = new LoadAndSave();
+        trainer = loadandsave.load("Worttrainer.txt");
+        assertEquals("Dog", trainer.getWortpaar(0).getWort());
+        assertEquals("https://www.hindustantimes.com/ht-img/img/2023/08/25/1600x900/international_dog_day_1692974397743_1692974414085.jpg", trainer.getWortpaar(0).getUrl());
+        assertEquals(14, trainer.getRichtig());
+        assertEquals(7, trainer.getFalsch());
+    }
+
+    @DisplayName("Tests if the Worttrainer is saved correctly")
+    @Test
+    public void testSave() throws IOException {
+        Worttrainer trainer1 = new Worttrainer();
+        Wort wort1= new Wort("Dog", "https://www.hindustantimes.com/ht-img/img/2023/08/25/1600x900/international_dog_day_1692974397743_1692974414085.jpg");
+        trainer1.addWort(wort1);
+        trainer1.setRichtig(14);
+        trainer1.setFalsch(7);
+        LoadAndSave ws= new LoadAndSave();
+        assertDoesNotThrow(()-> ws.save("Worttrainer.txt", trainer1), "Ungültiger Pfad");
+        Worttrainer trainer2 = ws.load("Worttrainer.txt");
+        assertEquals(trainer1.getWortpaar(0).getWort(), trainer2.getWortpaar(0).getWort());
+        assertEquals(trainer1.getWortpaar(0).getUrl(), trainer2.getWortpaar(0).getUrl());
+        assertEquals(trainer1.getRichtig(), trainer2.getRichtig());
+        assertEquals(trainer1.getFalsch(), trainer2.getFalsch());
+    }
+}
--- src/test/java/Worttest.java
+++ src/test/java/Worttest.java
@@ -9,38 +9,12 @@ import static org.junit.jupiter.api.Assertions.*;
  * @author Luka Condric
  * @version 2023-10-18
  */
-public class WortpaarTest {
+public class Worttest {
     @DisplayName("Tests if the constructor works")
     @Test
     public void testWort(){
         Wort wort= new Wort("wort","https://scitechdaily.com/images/Young-Humpback-Whale.jpg");
         assertThrows(IllegalArgumentException.class, () ->{wort.setWort("");});
         assertThrows(IllegalArgumentException.class, () -> {wort.setWort(null);});
-    }
-
-    @DisplayName("Testen ob ungültige URL erkannt wird")
-    @Test
-    public void testUrl() {
-        Wortpaar wp= new Wortpaar("wort", "https://www.tgm.ac.at/");
-        assertThrows(IllegalArgumentException.class, () ->{wp.setUrl("");});
-        assertThrows(IllegalArgumentException.class, () -> {wp.setUrl(null);});
-        assertThrows(IllegalArgumentException.class, () -> {wp.setUrl("Url");});
-    }
-    @DisplayName("Testen ob check eines un/gültigen URL funktioniert")
-    @Test
-    public void testWordValidierung() {
-        Wortpaar wp= new Wortpaar("wort", "https://www.tgm.ac.at/");
-        assertTrue(wp.checkWord("Auto"));
-        assertFalse(wp.checkWord(""));
-        assertFalse(wp.checkWord(null));
-    }
-
-    @DisplayName("Testen ob check einer un/gültigen URL funktioniert")
-    @Test
-    public void testUrlValidierung() {
-        Wortpaar wp= new Wortpaar("wort", "https://www.tgm.ac.at/");
-        assertTrue(wp.checkURL("https://www.wienerlinien.at/"));
-        assertFalse(wp.checkURL("wefgshdjkghljk"));
-        assertFalse(wp.checkURL(null));
     }
 }
